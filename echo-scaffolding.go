a123d5aca948f8c5a8fb63707ab703fc1d4d9711
cc <1234232@163.com>
2022-12-01 20:07:49 +0800

    update

--- echo-scaffolding.go
+++ echo-scaffolding.go
@@ -1,14 +1,6 @@
 package main
 
 import (
-	"fmt"
-
-	confnacos "github.com/echo-scaffolding/conf/nacos"
-
-	confini "github.com/echo-scaffolding/conf/ini"
-
-	confyaml "github.com/echo-scaffolding/conf/yaml"
-
 	"github.com/echo-scaffolding/router"
 
 	"github.com/echo-scaffolding/boot"
@@ -16,8 +8,5 @@ import (
 
 func main() {
 	boot.Boot()
-	fmt.Println(confnacos.NConfig())
-	fmt.Println(confyaml.YConf)
-	fmt.Println(confini.Config())
 	router.RunHttpServer()
 }
